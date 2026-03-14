2fe2ddeea352586654cf41e44cc77fc72fef3818
Pkb-Codes <krishnabhargavpuli@gmail.com>
2026-03-14 23:12:43 +0530

    options menu 50%
    added audio and video pages in options menu but buttons are not executed yet

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -1,15 +1,15 @@
 extends CanvasLayer
 
 @export var save_slots_scene: PackedScene
-@export var settings_scene: PackedScene
+@export var options_scene: PackedScene
 
 func _on_start_button_pressed() -> void:
 	if save_slots_scene:
 		get_tree().change_scene_to_packed(save_slots_scene)
 
 func _on_options_button_pressed() -> void:
-	if settings_scene:
-		get_tree().change_scene_to_packed(settings_scene)
+	var options_menu = options_scene.instantiate()
+	add_child(options_menu)
 
 func _on_quit_button_pressed() -> void:
 	get_tree().quit()
---
+++ scripts/options_menu.gd
@@ -0,0 +1,17 @@
+extends Panel
+
+
+func _on_key_binds_button_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_video_button_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_audio_button_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_back_button_pressed() -> void:
+	queue_free() #closes the options menu
