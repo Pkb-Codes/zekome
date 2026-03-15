57f2bb065afe54ee12571b2afc9af3ffc15b348d
Pkb-Codes <krishnabhargavpuli@gmail.com>
2026-03-15 12:37:43 +0530

    audio menu
    made the menus switch

--- scripts/options_menu.gd
+++ scripts/options_menu.gd
@@ -1,16 +1,40 @@
 extends Panel
 
+@onready var buttons = $button_container
+@onready var audio_page = $page_container/Audio_page
+@onready var video_page = $page_container/Video_page
+@onready var key_page = $page_container/Key_binds_page
+
+func _ready():
+	for page in $page_container.get_children():
+		page.connect("go_back", _back_to_menu)
+
+func show_page(active_page):
+	# hide the buttons
+	buttons.visible = false
+	#hide all pages
+	for page in $page_container.get_children():
+		page.visible = false
+	#make the active page visible
+	active_page.visible = true
+
+func _back_to_menu():
+	#hide all pages
+	for page in $page_container.get_children():
+		page.visible = false
+	#shoe buttons
+	buttons.visible = true
 
 func _on_key_binds_button_pressed() -> void:
-	pass # Replace with function body.
+	show_page(key_page)
 
 
 func _on_video_button_pressed() -> void:
-	pass # Replace with function body.
+	show_page(video_page)
 
 
 func _on_audio_button_pressed() -> void:
-	pass # Replace with function body.
+	show_page(audio_page)
 
 
 func _on_back_button_pressed() -> void:
