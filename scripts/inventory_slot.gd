bfe090145ffff07670b33d71191dc3bdbe8bce0b
karthikje7 <karthikje2503m@gmail.com>
2026-03-17 23:10:34 +0530

    additionalchanges(karthik)

--- scripts/inventory_slot.gd
+++ scripts/inventory_slot.gd
@@ -21,3 +21,36 @@ func _on_item_button_mouse_entered() -> void:
 
 func _on_item_button_mouse_exited() -> void:
 	pass # Replace with function body.
+
+
+func _on_equip_button_pressed() -> void:
+	$EquipSelectionPanel.show()
+	$UsagePanel.hide() # Hiding the Equip/Drop menu
+
+
+func _on_drop_button_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_button_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_button_2_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_button_3_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_button_4_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_button_5_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_button_6_pressed() -> void:
+	pass # Replace with function body.
